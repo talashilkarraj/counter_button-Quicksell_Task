01bdbfe12276093a06eff2a91c7144e69c6cb87a
talashilkarraj <2018.raj.talashilkar@ves.ac.in>
2021-09-01 18:52:48 +0530

    reduced redundancy, font updated

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import {useState} from 'react';
 
 function App() {
   const [count, setCount]=useState(1);
-  // const [data, setData]=useState(null);
 
 function getData(val){
-  val= Number(prompt());
+  val= Number(prompt("Enter a number (max 1000):"));
   val>1000? setCount(1000) : setCount(val);
-  // if (count>1000)
-  // {
-  //   setCount(1000);
-  // }
 }
 
   return (
     <div className="App">
       <div className="counter_components">
-        {/* <button onClick={()=>setCount(count === 0? count+1-1:count-1)} className="button1">-</button>
-        <div className="count" onClick={()=>{document.getElementsByClassName("count").innerHTML=prompt("number",)}}>{count}</div>
-        <button onClick={()=>setCount(count === 6? count+1-1:count+1)} className="button2">+</button> */}
         <div className="button_main">
           <button onClick={()=>setCount(count === 0? count:count-1)} className="button1">-</button>
-          <span style={{position:"relative", top:"20px"}} onClick={getData}>{count}</span>
+          <span style={{position:"relative", top:"14px"}} onClick={getData}>{count}</span>
           <button onClick={()=>setCount(count === 1000? count:count+1)} className="button2">+</button>
        </div>
       </div>
     </div>
   );
 }
-
-
 
 export default App;
