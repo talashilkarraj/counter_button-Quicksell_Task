a0d9ee7be8611cf6ff783ff4ca7582b06c1fd032
talashilkarraj <2018.raj.talashilkar@ves.ac.in>
2021-09-01 17:54:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'; 
 
 function App() {
+  const [count, setCount]=useState(1);
+  // const [data, setData]=useState(null);
+
+function getData(val){
+  val= Number(prompt());
+  val>1000? setCount(1000) : setCount(val);
+  // if (count>1000)
+  // {
+  //   setCount(1000);
+  // }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="counter_components">
+        {/* <button onClick={()=>setCount(count === 0? count+1-1:count-1)} className="button1">-</button>
+        <div className="count" onClick={()=>{document.getElementsByClassName("count").innerHTML=prompt("number",)}}>{count}</div>
+        <button onClick={()=>setCount(count === 6? count+1-1:count+1)} className="button2">+</button> */}
+        <div className="button_main">
+          <button onClick={()=>setCount(count === 0? count:count-1)} className="button1">-</button>
+          <span style={{position:"relative", top:"20px"}} onClick={getData}>{count}</span>
+          <button onClick={()=>setCount(count === 1000? count:count+1)} className="button2">+</button>
+       </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
